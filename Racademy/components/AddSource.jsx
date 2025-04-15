f2abb5896a69b7fb8ff2c6aac95d49455ee840a5
Eline-1007803 <1007803@hr.nl>
2025-04-15 23:52:46 +0200

    setup nativewind

---
+++ Racademy/components/AddSource.jsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+function AddSource () {
+    return (
+        <scrollview>
+            <Text className="text-3xl font-bold underline">Bron toevoegen</Text>
+        </scrollview>
+    )
+}
+
+export default AddSource
