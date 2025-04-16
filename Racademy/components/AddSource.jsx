47e96b347c6c211828a1da2b9408fd11db6d0c00
Eline-1007803 <1007803@hr.nl>
2025-04-16 11:32:55 +0200

    nativewind things

--- Racademy/components/AddSource.jsx
+++ Racademy/components/AddSource.jsx
@@ -1,11 +1,13 @@
-import { Text } from 'react-native';
+import {Text, ScrollView, View} from 'react-native';
 
 function AddSource () {
     return (
-        <scrollview>
-            <Text className="text-3xl font-bold underline">Bron toevoegen</Text>
-        </scrollview>
-    )
+        <ScrollView className="flex-1">
+            <View className="items-center justify-center bg-rac border rounded-lg shadow-lg shadow-neutral-300 ">
+                <Text className="text-5xl  font-bold underline ">Bron toevoegen</Text>
+            </View>
+        </ScrollView>
+    );
 }
 
 export default AddSource
