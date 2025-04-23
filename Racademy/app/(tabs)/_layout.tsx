7c86d25c515990c8e9d542e58a8df95c5e207449
Roos-1104659 <1104659@hr.nl>
2025-04-23 12:56:00 +0200

    added a registration screen to index so you can see

--- Racademy/app/(tabs)/_layout.tsx
+++ Racademy/app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+        <Tabs.Screen
+            name="registratiescherm"
+            options={{
+                title: 'Registreren',
+                tabBarIcon: ({ color }) => <IconSymbol size={28} name="heart.fill" color={color} />,
+            }}
+        />
     </Tabs>
   );
 }
--- Racademy/app/(tabs)/index.tsx
+++ Racademy/app/(tabs)/index.tsx
@@ -1,19 +1,45 @@
+import {StyleSheet} from 'react-native';
+
+import Registratiescherm from "@/components/registratiescherm";
 import AddSource from "@/components/AddSource";
 import Home from "@/components/Home";
 import {ScrollView} from "react-native";
 
 export default function HomeScreen() {
   return (
-      <ScrollView className="flex-1 bg-white">
-        <AddSource />
-      </ScrollView>
+      <>
+        <Registratiescherm/>
+
+      {/*<ScrollView className="flex-1 bg-white">*/}
+      {/*  <AddSource />*/}
+      {/*</ScrollView>*/}
+
+        </>
   );
 }
 
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
 
-export default function Hello() {
+export function Hello() {
     return (
-        <ScrollView classname="flex-1 bg-white">
+        <ScrollView className="flex-1 bg-white">
             <Home />
         </ScrollView>
     )
