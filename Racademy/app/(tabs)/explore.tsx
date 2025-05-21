5274fda15632b22dbac903ca9ce57acc7efc3754
Eline-1007803 <1007803@hr.nl>
2025-05-21 10:24:10 +0200

    explore for merge

--- Racademy/app/(tabs)/explore.tsx
+++ Racademy/app/(tabs)/explore.tsx
@@ -22,15 +22,3 @@ export default function Explore() {
   );
 }
 
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
