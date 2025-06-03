66edff4631650a804d267ad5ddce4170227845a9
Roos-1104659 <1104659@hr.nl>
2025-06-03 13:38:34 +0200

    updated users.py (wel nog met spellingsfouten)

--- backend/blueprints/users.py
+++ backend/blueprints/users.py
@@ -1,27 +1,37 @@
 from flask import Blueprint, request
 from flask_cors import cross_origin
+from werkzeug.security import generate_password_hash
+from backend.models.user_model import Users
+from flask import url_for
 
-from models.source_model import Sources
 
 users_bp = Blueprint('users_bp', __name__)
 
 @users_bp.post('/')
 @cross_origin()
-def create_source():
-    user_id = 1
-    title = request.json["title"]
-    description = request.json["description"]
-    link = request.json["link"]
-    isbn = request.json["isbn"]
-    image = request.json["image"]
-    print(request.json)
-    sources_model = Sources()
-    new_source = sources_model.add_sources(user_id, title, description, link, isbn, image)
-    return {'Successful!': new_source, 'success': True} , 201
+def create_user():
+    data = request.json
+    email = data.get("email")
+    teller = data.get("teller")
+    password = data.get("password")
+    if not all([email, teller, password]):
+        return {'error': 'Missing data'}, 400
+
+
 
 @users_bp.get('/')
 @cross_origin()
-def get_all_sources():
-    sources_model = Sources()
-    sources = sources_model.get_all_sources()
-    return {'sources': sources}
+def get_all_users():
+    user_model = Users()
+    users = user_model.get_all_users()
+    return {'users': users}
+
+
+@users_bp.get('/<int:user_id>')
+@cross_origin()
+def get_user_details(user_id):
+    user_model = Users()
+    user = user_model.get_user_by_id(user_id)
+    if not user:
+        return {'error': 'User not found'}, 404
+    return {'user': user}
