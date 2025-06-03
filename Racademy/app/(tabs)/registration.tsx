52167ae9a50f37e89c925068bb819374ea738a28
Roos-1104659 <1104659@hr.nl>
2025-06-03 13:33:34 +0200

    deleted useless code

--- Racademy/app/(tabs)/registration.tsx
+++ Racademy/app/(tabs)/registration.tsx
@@ -9,28 +9,7 @@ export default function Explore() {
        <>
         <Registratiescherm/>
 
-      {/*<ScrollView className="flex-1 bg-white">*/}
-      {/*{  <AddSource /> }*/}
-      {/*</ScrollView>*/}
         <TabLayout/>
         </>
   );
 }
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
