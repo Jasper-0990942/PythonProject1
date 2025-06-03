c16b65511cc12dc5fbf8032d516c8c56d6044c0b
Roos-1104659 <1104659@hr.nl>
2025-06-03 13:33:11 +0200

    updated registratiescherm

--- Racademy/components/registratiescherm.tsx
+++ Racademy/components/registratiescherm.tsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import {View, Text, TextInput, StyleSheet, Alert, Pressable} from 'react-native';
 
 export default function Registratiescherm() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    const [studentnr, setStudentnr] = useState<number | undefined>(undefined);
+    const [geboortedatum, setGeboortedatum] = useState('');
+    const [voornaam, setVoornaam] = useState('');
+    const [achternaam, setAchternaam] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const aanmaakRegistratie = () => {
-        if (!name || !email || !password || !confirmPassword) {
+    const aanmaakRegistratie = async () => {
+        if (!studentnr || !geboortedatum || !password || !confirmPassword || !voornaam || !achternaam) {
             Alert.alert("Vul alle velden in.")
             return;
         }
@@ -16,23 +20,74 @@ export default function Registratiescherm() {
             Alert.alert("Wachtwoorden komen niet overeen.");
             return;
         }
+        if (!studentnr || studentnr.toString().length !== 7) {
+            Alert.alert("Studentnummer moet precies 7 cijfers bevatten.");
+            return;
+        }
+        try {
+            const response = await fetch('http://127.0.0.1:5000/', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json',},
+                body: JSON.stringify({
+                    voornaam: voornaam,
+                    achternaam: achternaam,
+                    studentnummer: studentnr,
+                    geboortedatum: geboortedatum,
+                    wachtwoord: password,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error('Registratie mislukt');
+            }
 
-        Alert.alert('Gelukt!', `Welkom ${name}!`);
+            Alert.alert('Gelukt!', `Welkom ${voornaam}!`);
+            navigation.navigate('Login');
+        } catch (error) {
+            Alert.alert('Fout', 'Er ging iets mis bij het registreren.');
+        }
     };
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Registreren</Text>
-            <Text className="text-s">Naam:</Text>
+            {/*<Text className="text-s">Naam:</Text>*/}
+            {/*<TextInput style={styles.input}*/}
+            {/*placeholder={'Naam'}*/}
+            {/*value={name}*/}
+            {/*onChangeText={setName}>*/}
+            {/*</TextInput>*/}
+            {/*<Text className="text-s">Email:</Text>*/}
+            {/*<TextInput style={styles.input}*/}
+            {/*placeholder={'Email'}*/}
+            {/*value={email}*/}
+            {/*onChangeText={setEmail}>*/}
+            {/*</TextInput>*/}
+            <Text className="text-s">Studentnummer:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder={"Studentnummer"}
+                value={studentnr?.toString() || ''}
+                onChangeText={(text) => {
+                    const filtered = text.replace(/[^0-9]/g, '');
+                    if (filtered.length <= 7) {
+                        setStudentnr(filtered ? parseInt(filtered, 10) : undefined);}}}
+                keyboardType="numeric"/>
+            <Text className="text-s">Geboortedatum:</Text>
             <TextInput style={styles.input}
-            placeholder={'Naam'}
-            value={name}
-            onChangeText={setName}>
+                       placeholder={'Geboortedatum'}
+                       value={geboortedatum}
+                       onChangeText={setGeboortedatum}>
             </TextInput>
-            <Text className="text-s">Email:</Text>
+            <Text className="text-s">Voornaam:</Text>
             <TextInput style={styles.input}
-            placeholder={'Email'}
-            value={email}
-            onChangeText={setEmail}>
+                       placeholder={'Voornaam'}
+                       value={voornaam}
+                       onChangeText={setVoornaam}>
+            </TextInput>
+            <Text className="text-s">Achternaam:</Text>
+            <TextInput style={styles.input}
+                       placeholder={'Achternaam'}
+                       value={achternaam}
+                       onChangeText={setAchternaam}>
             </TextInput>
             <Text className="text-s">Wachtwoord:</Text>
             <TextInput style={styles.input}
