00c2e50654d5d837782c3e57c8a77e163c4c0d64
Roos-1104659 <1104659@hr.nl>
2025-06-04 07:09:32 +0200

    changed port bc of issues

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,8 @@ from blueprints.users import users_bp
 from blueprints.sources import sources_bp
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+CORS(app, origins="*", supports_credentials=True)
 
 app.register_blueprint(sources_bp, url_prefix="/sources")
 app.register_blueprint(users_bp, url_prefix="/users")
@@ -13,6 +14,6 @@ app.register_blueprint(users_bp, url_prefix="/users")
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5050, host='0.0.0.0', debug=True)
 
 
