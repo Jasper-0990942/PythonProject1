3c39db2b0055ab044f84d6e2f2759f4605a7caa4
Roos-1104659 <1104659@hr.nl>
2025-06-13 22:07:10 +0200

    deleted password hash

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash
 from models.database_connection import Database
 
 class Users:
