80dfdf1e47771ee4239d92660368638fa793e535
Jasper <0990942@hr.nl>
2025-06-23 16:49:16 +0200

    fixed registratie dooverwijzing naar login

--- Racademy/app/Registratiescherm.tsx
+++ Racademy/app/Registratiescherm.tsx
@@ -56,7 +56,7 @@ export default function Registratiescherm() {
             }
 
             alert('Gelukt! Welkom ${fname}!');
-            router.push('../login');
+            router.push('/login');
         } catch (error) {
             console.error("FOUTTTT", error);
             alert('Er ging iets mis bij het registreren.');
